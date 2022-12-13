7daa8fe535d4fc2a3da751d3566aed947c83d9e9
Mukund Patel <80103805+mukund1311@users.noreply.github.com>
2022-12-13 06:43:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React, { Component, useState } from "react";
+import  Heading from './components/Heading'
+import  SubHeading from './components/SubHeading'
+import  InputQuery from './components/InputQuery'
+import  SubmitButton from './components/SubmitButton'
 import '../styles/App.css';
 
 export default function App() {
